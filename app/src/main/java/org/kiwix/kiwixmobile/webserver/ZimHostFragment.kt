233c07fe6d7033804cb7f74d4fafc72d3f4daeb4
MohitMali <futuretechnoworld.mohit@gmail.com>
2023-01-27 17:53:35 +0530

    Checkbox always visible to user in Wifi Hotspot and user can select and unselect zim files when server already started

--- app/src/main/java/org/kiwix/kiwixmobile/webserver/ZimHostFragment.kt
+++ app/src/main/java/org/kiwix/kiwixmobile/webserver/ZimHostFragment.kt
@@ -82,7 +82,6 @@ class ZimHostFragment : BaseFragment(), ZimHostCallbacks, ZimHostContract.View {
   private lateinit var serviceConnection: ServiceConnection
   private var progressDialog: ProgressDialog? = null
   private var activityZimHostBinding: ActivityZimHostBinding? = null
-  private var allBooks: List<BooksOnDiskListItem>? = null
   private val selectedBooksPath: ArrayList<String>
     get() {
       return booksAdapter.items
@@ -99,16 +98,6 @@ class ZimHostFragment : BaseFragment(), ZimHostCallbacks, ZimHostContract.View {
           }
         }
         as ArrayList<String>
-    }
-
-  private val selectedBooks: List<BooksOnDiskListItem>
-    get() {
-      return booksAdapter.items
-        .filter(BooksOnDiskListItem::isSelected)
-        .filterIsInstance<BookOnDisk>()
-        .map {
-          it
-        }
     }
 
   override fun onCreateView(
@@ -277,6 +266,9 @@ class ZimHostFragment : BaseFragment(), ZimHostCallbacks, ZimHostContract.View {
     }
     booksAdapter.items = booksList
     saveHostedBooks(booksList)
+    if (ServerUtils.isServerStarted) {
+      startWifiHotspot(true)
+    }
   }
 
   override fun onStart() {
@@ -331,8 +323,7 @@ class ZimHostFragment : BaseFragment(), ZimHostCallbacks, ZimHostContract.View {
     activityZimHostBinding?.startServerButton?.setBackgroundColor(
       resources.getColor(R.color.stopServerRed)
     )
-    booksAdapter.items = selectedBooks
-    bookDelegate.selectionMode = SelectionMode.NORMAL
+    bookDelegate.selectionMode = SelectionMode.MULTI
     booksAdapter.notifyDataSetChanged()
   }
 
@@ -358,7 +349,6 @@ class ZimHostFragment : BaseFragment(), ZimHostCallbacks, ZimHostContract.View {
     activityZimHostBinding?.startServerButton?.setBackgroundColor(
       resources.getColor(R.color.startServerGreen)
     )
-    allBooks?.let { booksAdapter.items = it }
     bookDelegate.selectionMode = SelectionMode.MULTI
     booksAdapter.notifyDataSetChanged()
   }
@@ -433,15 +423,18 @@ class ZimHostFragment : BaseFragment(), ZimHostCallbacks, ZimHostContract.View {
 
   override fun addBooks(books: List<BooksOnDiskListItem>) {
     booksAdapter.items = books
-    allBooks = books
   }
 
   override fun onIpAddressValid() {
     progressDialog?.dismiss()
+    startWifiHotspot(false)
+  }
+
+  private fun startWifiHotspot(restartServer: Boolean) {
     requireActivity().startService(
       createHotspotIntent(ACTION_START_SERVER).putStringArrayListExtra(
         SELECTED_ZIM_PATHS_KEY, selectedBooksPath
-      )
+      ).putExtra(RESTART_SERVER, restartServer)
     )
   }
 
@@ -452,6 +445,7 @@ class ZimHostFragment : BaseFragment(), ZimHostCallbacks, ZimHostContract.View {
 
   companion object {
     const val SELECTED_ZIM_PATHS_KEY = "selected_zim_paths"
+    const val RESTART_SERVER = "restart_server"
     const val PERMISSION_REQUEST_CODE_COARSE_LOCATION = 10
   }
 }
